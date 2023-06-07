3e5720cfbca450cb0755abe8b8dedc7eced91ef2
Ananyathakur01 <134470131+Ananyathakur01@users.noreply.github.com>
2023-06-07 12:16:50 +0530

    Add files via upload

---
+++ 4) senior_citizen.py
@@ -0,0 +1,7 @@
+a=eval(input("enter your date of year\n"))
+c=2023-a
+d=1020-((1020/100)*20)
+if c>60:
+    print("you are above 60 so the fair is",d)
+else:
+    print("you are below 60 so the fair is")
