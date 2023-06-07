80f54f038c67c779580ceb60788fc4df2f7e625e
Ananyathakur01 <134470131+Ananyathakur01@users.noreply.github.com>
2023-06-07 10:43:38 +0530

    Add files via upload

---
+++ 3) check_the_PowerofaNumber.py
@@ -0,0 +1,7 @@
+a=eval(input("enter any number\n"))
+b=eval(input("Enter the power of that number\n"))
+c=a^b
+if c%2==0:
+    print(f"the power of {a} is even\n")
+else:
+    print(f"the power of {a} is odd") 
