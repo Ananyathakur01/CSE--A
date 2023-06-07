c2e393d4a4eb11d7f8519a481423af8d50792820
Ananyathakur01 <134470131+Ananyathakur01@users.noreply.github.com>
2023-06-07 12:32:57 +0530

    Add files via upload

---
+++ 6) blood_donation.py
@@ -0,0 +1,7 @@
+a=input("enter your name\n")
+b=int(input("enter your age\n"))
+c=eval(input("enter your weight\n"))
+if (b>18 and c>40):
+    print("criteria matched you are eligible for boold donation")
+else:
+    print("you are not eligible for blood donation")
